79cd3f8302b1cb0037a5b44f1f500be8afd48be8
rishurai18 <rairishuk@gmail.com>
2025-06-30 12:10:09 +0000

    after some learn

---
+++ myapp/src/module1.mjs
@@ -0,0 +1,10 @@
+import ui from "./module2.mjs"
+import {b,c,d} from "./module2.mjs"
+
+console.log(ui);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+
---
+++ myapp/src/module2.mjs
@@ -0,0 +1,10 @@
+const a = "Sumant Rai"
+const b = "Apeksha More"
+const c = "Rajkumar pal"
+const d = "Aniket Patel"
+
+export default(a);
+
+export {c}
+export {d}
+export {b}
